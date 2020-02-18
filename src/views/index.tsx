b83ab674a2374714e8efda039c67c0f3bc1ea6a5
Shawn Toubeau <shawntoubeau@gmail.com>
2020-02-18 17:23:15 -0500

    Cleaned up root view & styles

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import Grid from '../components/Grid/Grid';
+import './index.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className="root">
         <h1 className="title">Hello World</h1>
-        <div style={{ border: '1px solid orangered' }}>
-          <Grid gridSize={{ x: 10, y: 10 }} />
-        </div>
+        <Grid gridSize={{ x: 10, y: 10 }} />
       </div>
     );
   }
