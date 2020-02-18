a803a9e4c4e291f707933c7052e239edc3520e30
Shawn Toubeau <shawntoubeau@gmail.com>
2020-02-18 15:42:54 -0500

    Adds Grid component

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,19 +1,13 @@
-import * as React from "react";
-import { Canvas } from "react-three-fiber";
-import Box from "../components/Box";
+import * as React from 'react';
+import Grid from '../components/Grid/Grid';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">Hello World</h1>
-        <div style={{ border: "1px solid orangered" }}>
-          <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-          </Canvas>
+        <div style={{ border: '1px solid orangered' }}>
+          <Grid gridSize={{ x: 10, y: 10 }} />
         </div>
       </div>
     );
