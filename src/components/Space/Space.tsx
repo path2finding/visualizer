c260313d92abcb6ed470b68757089d8b0494f4c7
Jame <coynej1@wit.edu>
2020-03-17 11:00:17 -0400

    added empty component, as well as various visual features such as orbit controls, and a background grid

--- src/components/Space/Space.tsx
+++ src/components/Space/Space.tsx
@@ -1,38 +1,62 @@
 import * as THREE from 'three';
-import React, { useRef, useState, MutableRefObject, Suspense } from 'react';
-import { useLoader } from 'react-three-fiber';
-import { Mesh } from 'three';
+import React, { useRef, useState, MutableRefObject, Suspense, useEffect } from 'react';
+import { useLoader, useFrame, stateContext } from 'react-three-fiber';
+import { Mesh, Vector3 } from 'three';
 
 // Interfaces
 import { SpaceState } from '../../models/space';
 
-const GenericSpace: React.FC<BoxProps> = props => {
-  console.log(props);
-  const texture = useLoader(
+const Wall: React.FC<BoxProps> = props => {
+
+    const texture = useLoader(
     THREE.TextureLoader,
     `${process.env.PUBLIC_URL}${props.type}.png`
   );
-  const mesh: MutableRefObject<Mesh | undefined> = useRef();
 
-  if (props.type === 'empty') {
-    return <mesh></mesh>;
-  }
-
-  return (
-    <mesh ref={mesh}>
+  return(
+    <mesh>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshPhongMaterial attach="material" map={texture} />
     </mesh>
   );
+}
+
+const Empty: React.FC<BoxProps> = props => {
+
+  return(
+       <mesh position={new Vector3(0,-0.5,0)} rotation={[-(Math.PI/2),0,0]} >
+          <planeBufferGeometry attach="geometry" args={[1, 1, 1, 1]}/>
+          <meshPhongMaterial attach="material"color={ props.visited ? props.path ? 'red': 'yellow' : 'blue' }/>
+      </mesh>
+  );
+}
+
+const GenericSpace: React.FC<BoxProps> = props => {
+
+  const mesh: MutableRefObject<Mesh | undefined> = useRef();
+
+  if (props.type === 'empty') {
+    return(
+      <Empty visited={props.visited} path={props.path}/>
+    );
+  }
+  else{
+    return (
+      <Suspense fallback="none"> 
+        <Wall type={props.type}/>
+      </Suspense>
+    );
+  }
 };
 
 const Space: React.FC<BoxProps> = props => {
+
   const [hovered, setHover] = useState(false);
 
   const spaceClicked = () => {
     console.log('space clicked');
   };
-
+  
   return (
     <mesh
       position={props.position}
@@ -40,9 +64,14 @@ const Space: React.FC<BoxProps> = props => {
       onPointerOver={e => setHover(true)}
       onPointerOut={e => setHover(false)}
     >
-      <Suspense fallback="none">
-        <GenericSpace type={props.type} />
-      </Suspense>
+        <GenericSpace type={props.type} visited={props.visited} path={props.path}/>
+        {
+          hovered && 
+          <mesh>
+            <boxBufferGeometry attach="geometry" args={[1.0001, 1.0001, 1.0001]} />
+            <meshPhongMaterial attach="material" wireframe={true}/>
+          </mesh>
+        }
     </mesh>
   );
 };
@@ -50,6 +79,8 @@ const Space: React.FC<BoxProps> = props => {
 export interface BoxProps {
   position?: number[];
   type?: string;
+  visited?: Boolean;
+  path?: Boolean;
 }
 
 export default Space;
